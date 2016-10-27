6c7171220a164d30870d1f3f2cde952617716a27
purples <ljj92510@163.com>
2016-10-27 11:00:46 +0800

    Initial Commit

---
+++ ScrollCycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollCycle
+//
+//  Created by 李景景 on 16/10/27.
+//  Copyright © 2016年 Linkstores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollCycle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollCycle
+//
+//  Created by 李景景 on 16/10/27.
+//  Copyright © 2016年 Linkstores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
